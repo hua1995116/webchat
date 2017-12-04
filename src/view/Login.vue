6ffac6174ff10894812f2df0f0cc250cde2cd1cc
hua1995116 <461249104@qq.com>
2017-12-04 17:53:57 +0800

    '增加监控'

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -51,6 +51,10 @@
             this.getSvgModal.$root.$options.clear()
             this.$store.commit('setSvgModal', null)
             this.$router.push({path: '/'})
+          } else {
+            Alert({
+              content: res.data.data
+            })
           }
           document.form2.reset()
         } else {
