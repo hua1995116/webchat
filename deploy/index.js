468192e24f5d1527efe8e64d7f8e04acb1ffcfe4
hua1995116 <461249104@qq.com>
2018-03-22 11:50:06 +0800

    '修复启动bug以及上传路径'

--- deploy/index.js
+++ deploy/index.js
@@ -45,8 +45,8 @@ function readFileSync() {
         //     if (err) throw err;
         //     console.log('The file has been saved!');
         // });
-        const rootJs = path.join(process.cwd(), './dist/static/js');
-        const rootCss = path.join(process.cwd(), './dist/static/css');
+        const rootJs = path.join(process.cwd(), '../dist/static/js');
+        const rootCss = path.join(process.cwd(), '../dist/static/css');
         const exportList = exportUrlList(rootJs, JSfileNames).concat(exportUrlList(rootCss, CSSfileNames));
         upload(exportList);
     });
