1ff825224b97da5b97102db8b59e002c5800d2b3
huayifeng <461249104@qq.com>
2018-09-08 22:53:54 +0800

    fix: env

--- config/client.js
+++ config/client.js
@@ -1,4 +1,4 @@
-const isLocal = window.location.href.indexOf('local') || false
+const isLocal = window.location.href.indexOf('debug') !== -1 || false
 
 export default {
     server: (process.env.NODE_ENV === 'development' || isLocal )?  'http://127.0.0.1:9090/' : 'http://www.qiufengh.com:9090/',
