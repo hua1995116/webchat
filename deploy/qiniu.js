1ec8c09ebc4c43f504626d0dd46d8d314e6e5724
huayifeng <461249104@qq.com>
2018-10-23 23:42:32 +0800

    fix: s3

--- deploy/qiniu.js
+++ deploy/qiniu.js
@@ -1,7 +1,7 @@
 const qiniuNode= require('qiniu-node');
 const config = require('./config');
-const qiniu = new qiniuNode(config);
 
 module.exports = function(fileList) {
+    const qiniu = new qiniuNode(config);
     return qiniu.upload(fileList);
 }
