8f8c1034517404246182d4918a8e5ceec41bf946
huayifeng <461249104@qq.com>
2018-11-18 18:38:32 +0800

    feat: node cache for unreadList

--- server_modules/cache.js
+++ server_modules/cache.js
@@ -1,26 +1,80 @@
 const cache_data = {};
 
-class RedisCache {
-    constructor() {
-        this.cache_data = {};
-    }
-    inrcCache() {
-
-    }
-    updateCache() {
-
-    }
-    getCacheById() {
-
-    }
-    deleteCacheById(){
-
-    }
-    getAllCache() {
-
-    }
-    resetCacheById() {
-
-    }
+function inrcCache(key) {
+    return new Promise((resv, rej) => {
+        if(!cache_data[key]) {
+            cache_data[key] = 0;
+        }
+        cache_data[key]++
+        resv(1);
+    })
 }
 
+function getCacheById(key) {
+    return new Promise((resv, rej) => {
+        const res = cache_data[key] ? cache_data[key] : 0;
+        resv(res);
+    })
+    
+}
+
+function updateCache(key, value) {
+    return new Promise((resv, rej) => {
+        cache_data[key] = value;
+        resv(1);
+    })
+}
+
+function deleteCacheById(key) {
+    return new Promise((resv, rej) => {
+        if(cache_data[key]) {
+            delete cache_data[key]
+        }
+        resv(1);
+    })
+}
+
+function resetCacheById(key) {
+    return new Promise((resv, rej) => {
+        cache_data[key] = 0;
+        resv(1);
+    })
+}
+
+function updatehCache(id, key, value) {
+    return new Promise((resv, rej) => {
+        if(!cache_data[id]) {
+            cache_data[id] = {};
+        }
+        cache_data[id][key] = value;
+        resv(1);
+    })
+}
+
+function gethCacheById(id, key) {
+    return new Promise((resv, rej) => {
+        let res = 0;
+        if(cache_data[id]) {
+            res = cache_data[id][key];   
+        }
+        resv(res);
+    })
+}
+
+function gethAllCache(id) {
+    return new Promise((resv, rej) => {
+        const res = Object.keys(cache_data[id]);
+        resv(res);
+    })
+}
+
+module.exports = {
+    inrcCache,
+    getCacheById,
+    updateCache,
+    deleteCacheById,
+    resetCacheById,
+    updatehCache,
+    gethCacheById,
+    gethAllCache
+};
