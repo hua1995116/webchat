27669eee31c3a04e4d603a2f06929fcfa5c6e38b
huayifeng <461249104@qq.com>
2019-06-01 19:51:57 +0800

    fix: key

--- deploy/config.js
+++ deploy/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    accessKey: '6Ez3lyFtAlwg01yLHNJfR-q88bcmwrgv4jsHGUAv',
-    secretKey: 'EVZX8Jpdk2lVGHvHxvF6jz8IvrrMR5YOWPLWMrvF',  // key
+    accessKey: '',
+    secretKey: '',  // key
     zone: 'Zone_z2',
     bucket: 'chat',   // bucket name
     dir: 'webchat/',     //
